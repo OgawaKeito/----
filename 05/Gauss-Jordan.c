4fa06b16adb4586c2d0d7f96600726e1bf17cc4c
OgawaKeito <program@okei-studio.work>
2023-11-09 10:46:44 +0900

    20231109

--- 05/Gauss-Jordan.c
+++ 05/Gauss-Jordan.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 
-double matrix[3][4] =
+#define X 4
+#define Y 3
+
+double matrix[Y][X] =
     {
-        {1, 3, -4, 5},
-        {5, 4, 2, 14},
-        {1, -3, 5, 5}};
+        {-2, -1, 6, 14},
+        {-1, 5, -2, 3},
+        {5, -1, 2, 6}};
 
 void swap(void);
 void normalize(void);
 void Print_Arr(void);
 
-void main(void)
+int main(void)
 {
     Print_Arr();
     swap();
     Print_Arr();
     normalize();
     Print_Arr();
+
+    return 0;
 }
 
 // 入れ替え
 void swap(void)
 {
     int temp1, temp2;
-    double temp[4];
-    for (int i = 0; i < 3; i++)
+    double temp[X];
+    for (int i = 0; i < Y; i++)
     {
         if (matrix[i][i] == 0)
         {
-            temp1 = (i + 1) % 3;
-            temp2 = (i + 2) % 3;
+            temp1 = (i + 1) % Y;
+            temp2 = (i + 2) % Y;
 
             if (matrix[temp1][i] != 0)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < X; j++)
                 {
                     temp[j] = matrix[i][j];
                     matrix[i][j] = matrix[temp1][j];
@@ -42,7 +47,7 @@ void swap(void)
             }
             else if (matrix[temp2][i] != 0)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < X; j++)
                 {
                     temp[j] = matrix[i][j];
                     matrix[i][j] = matrix[temp2][j];
@@ -57,22 +62,21 @@ void swap(void)
 void normalize(void)
 {
     double temp;
-    for (int k = 0; k < 3; k++)
+    for (int k = 0; k < Y; k++)
     {
         temp = matrix[k][k];
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < X; i++)
         {
             matrix[k][i] /= temp;
         }
-
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < Y; j++)
         {
             if (j == k)
             {
                 continue;
             }
             temp = matrix[j][k];
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < X; i++)
             {
                 matrix[j][i] -= temp * matrix[k][i];
             }
@@ -83,9 +87,9 @@ void normalize(void)
 // 行列表示
 void Print_Arr(void)
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < Y; i++)
     {
-        printf("{%8.5f,%8.5f,%8.5f,%8.5f}\n", matrix[i][0], matrix[i][1], matrix[i][2], matrix[i][3]);
+        printf("{%7.3f,%7.3f,%7.3f,%7.3f}\n", matrix[i][0], matrix[i][1], matrix[i][2], matrix[i][3]);
     }
     printf("\n");
 }
